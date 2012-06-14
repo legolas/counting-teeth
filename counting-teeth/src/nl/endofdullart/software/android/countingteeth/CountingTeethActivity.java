073f2f47e198025830ea08c8753185bfbd27383d
legolas <legolasgreenlf@gmail.com>
2012-06-14 09:15:41 +0200

    Had eclipse generate the app and test project files.

---
+++ counting-teeth/src/nl/endofdullart/software/android/countingteeth/CountingTeethActivity.java
@@ -0,0 +1,13 @@
+package nl.endofdullart.software.android.countingteeth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CountingTeethActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
